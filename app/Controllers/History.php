e8fa48d431ad4d2c7ab36654937cb649097c5630
Rizki Ramadhan <farazrizki13@gmail.com>
2020-10-16 10:46:06 +0700

    mengubah history.php

--- app/Controllers/History.php
+++ app/Controllers/History.php
@@ -2,13 +2,23 @@
 
 namespace App\Controllers;
 
+use App\Models\TiketOrderModel;
+
 class History extends BaseController
 {
+    protected $TiketOrderModel;
+    public function __construct()
+    {   
+        $this->TiketOrderModel = new TiketOrderModel();
+    }
+
     public function index()
     {
+        $tiketHistory = $this->TiketOrderModel->getMyOrder(session()->get('id'));
         $data = [
             "title" => "Ktrains | History",
-            "active" => "history"
+            "active" => "history",
+            "history" => $tiketHistory
         ];
         return view('home/history', $data);
     }
--- app/Views/home/history.php
+++ app/Views/home/history.php
@@ -4,23 +4,20 @@
 <!-- main -->
 
 <div class="container my-5" style="min-height: 400px">
-    <div class="row">
-        <div class="col-12">
-            <h3>Histori Transaksi</h3>
-        </div>
+  <div class="row">
+    <div class="col-12">
+      <h3>Histori Transaksi</h3>
     </div>
-    <div class="row">
-        <div class="col-12">
-            <hr class="bg-primary" style="height: 2px" />
-        </div>
+  </div>
+  <div class="row">
+    <div class="col-12">
+      <hr class="bg-primary" style="height: 2px" />
     </div>
-    <div class="row my-3" style="display: none" id="noHistory">
-        <div class="col-12 text-center">
-            <h1 class="font-italic">You dont have history yet :(</h1>
-        </div>
-    </div>
-    <div class="row">
-    <div class="col-md-4 mb-3 card-container">
+  </div>
+  <?php if ($history) : ?>
+    <?php foreach ($history as $key) : ?>
+      <div class="row">
+        <div class="col-md-4 mb-3 card-container">
           <div class="card shadow-sm">
             <div class="card-body">
               <div class="row">
@@ -28,22 +25,22 @@
                   <p class="font-weight-bold">Histori Tiket</p>
                 </div>
                 <div class="col-6 text-right">
-                  <i class="fa fa-times point delete" id="1"></i>
+                  <i class="fa fa-times point delete" id="<?= $key['id']; ?>"></i>
                 </div>
               </div>
               <div class="row">
                 <div class="col-5">No pesanan</div>
-                <div class="col-2 text-center">:</div>
-                <div class="col-5 text-right">1598190426</div>
+                <div class="col-1 text-center">:</div>
+                <div class="col-6 text-right"><?= $key['no_pesanan']; ?></div>
               </div>
               <div class="row">
                 <div class="col-5">status</div>
-                <div class="col-2 text-center">:</div>
-                <div class="col-5 text-right text-success">Lunas</div>
+                <div class="col-1 text-center">:</div>
+                <div class="col-6 text-right text-success">Lunas</div>
               </div>
               <div class="row mt-3">
                 <div class="col-12 text-right">
-                  <p class="text-primary point lihatTiket" id="1">
+                  <p class="text-primary point lihatTiket" id="<?= $key['id']; ?>">
                     Lihat Tiket
                   </p>
                 </div>
@@ -51,7 +48,15 @@
             </div>
           </div>
         </div>
+      </div>
+    <?php endforeach ?>
+  <?php else : ?>
+    <div class="row my-3" id="noHistory">
+      <div class="col-12 text-center">
+        <h1 class="font-italic">You dont have history yet :(</h1>
+      </div>
     </div>
+  <?php endif ?>
 </div>
 
 <!-- /main -->
@@ -59,22 +64,22 @@
 <!-- * restore -->
 
 <div class="container">
-    <div class="row justify-content-end">
-       <div class="shadow p-3" id="restore" data-id="index.html">
-          <img src="img/restore.png" width="35" height="35" />
-          <span class="ml-3 font-weight-bold">Restore Tiket</span>
-        </div>
+  <div class="row justify-content-end">
+    <div class="shadow p-3" id="restore" data-id="index.html">
+      <img src="img/restore.png" width="35" height="35" />
+      <span class="ml-3 font-weight-bold">Restore Tiket</span>
     </div>
+  </div>
 </div>
 
-    <!-- * /restore -->
+<!-- * /restore -->
 
-    <!-- * Tiket -->
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-6 p-2 position-absolute" id="theTiketHistory"></div>
-      </div>
-    </div>
+<!-- * Tiket -->
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-6 p-2 position-absolute" id="theTiketHistory"></div>
+  </div>
+</div>
 <!-- * /Tiket -->
 
 <?= $this->endSection(); ?>
