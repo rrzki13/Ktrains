d18714c8828df89ade7710bb0fa2d5345e01e138
Rizki Ramadhan <farazrizki13@gmail.com>
2020-10-26 18:40:04 +0700

    mengubah footer

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,33 +1,45 @@
 <!-- footer -->
-<footer div class="footer">
+<div class="foot w-100">
     <div class="container">
-        <div class="container2">
-            <div class="left-side">
+        <div class="row p-5">
+            <div class="col-md-4" id="left">
+                <div class="row">
+                    <h3 class="none-in-md">
+                        <i class="fas fa-globe-asia text-center" style="margin-right: 10px"></i>Ktrains
+                    </h3>
+                    <p class="none-in-md">Train Ticket</p>
+                    <div class="col-9 text-center show-in-md2">
+                        <i class="fas fa-globe-asia text-center" style="font-size: 100px;"></i>
+                    </div>
+                    <div class="col-9 text-center show-in-md2 mt-3">
+                        <h1>Ktrains</h1>
+                        <span>Train tickets</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3" id="mid">
                 <h3>
-                    <i class="fas fa-globe-asia" style="margin-right: 10px"></i>Ktrains
+                    Follow Us
                 </h3>
-                <p>Train Ticket</p>
+                <p>Thanks to visited</p>
             </div>
-            <div class="right-side">
-                <h3>Follow Us</h3>
-                <p style="transform: translateX(-40px)">Thanks For Your Visit</p>
-            </div>
-            <div class="wrapper">
-                <div class="button">
-                    <div class="icon"><i class="fab fa-facebook-f"></i></div>
-                    <label>Facebook</label>
-                </div>
-                <div class="button">
-                    <div class="icon"><i class="fab fa-twitter"></i></div>
-                    <label>Twitter</label>
-                </div>
-                <div class="button">
-                    <div class="icon"><i class="fab fa-instagram"></i></div>
-                    <label>Instagram</label>
+            <div class="col-5" id="right">
+                <div class="wrap">
+                    <div class="button shadow">
+                        <div class="icon"><i class="fab fa-facebook-f"></i></div>
+                        <label class="label">Facebook</label>
+                    </div>
+                    <div class="button shadow">
+                        <div class="icon"><i class="fab fa-instagram"></i></div>
+                        <label class="label">Instagram</label>
+                    </div>
+                    <div class="button shadow">
+                        <div class="icon"><i class="fab fa-twitter"></i></div>
+                        <label class="label">Twitter</label>
+                    </div>
                 </div>
             </div>
-            <hr class="footerLine" />
         </div>
     </div>
-</footer>
+</div>
 <!-- /footer -->
