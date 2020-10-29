57f58ec9898e2fc34b4206b0613d2d9eab31db1f
Rizki Ramadhan <farazrizki13@gmail.com>
2020-10-29 11:42:33 +0700

    memperbaiki tampilan manager

--- app/Views/layout/sidebarManager.php
+++ app/Views/layout/sidebarManager.php
@@ -11,7 +11,7 @@
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="<?= base_URL(); ?>/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image"/>
+                <img src="<?= base_URL(); ?>/profile/<?= session()->get("gambar"); ?>" class="img-circle elevation-2" alt="User Image"/>
             </div>
             <div class="info">
                 <a href="#" target="blank" class="d-block"><?= session()->get("username"); ?></a>
