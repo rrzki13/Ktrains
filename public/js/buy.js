be10c22b1063181b5e1f2774a6d451ab4940597b
Rizki Ramadhan <farazrizki13@gmail.com>
2020-11-16 09:33:44 +0700

    debug

--- public/js/buy.js
+++ public/js/buy.js
@@ -3,6 +3,8 @@ const formatter = new FormatMoney();
 const change = formatter.toRupiah(get("#total").textContent);
 let data = [];
 let data2 = [];
+const gerbong = [];
+const gerbong2 = [];
 
 function jml_gerbong_test() {
   $.ajax({
@@ -13,6 +15,7 @@ function jml_gerbong_test() {
     dataType: "json",
     success: function (result) {
       let jml_gerbong_test = result.data[0].jml_gerbong;
+      gerbong.push(jml_gerbong_test);
       get("#jml_gerbong").innerHTML = jml_gerbong_test;
     },
   });
@@ -27,6 +30,7 @@ if (get("#tglPulang")) {
     dataType: "json",
     success: function (result) {
       let jml_gerbong_test = result.data[0].jml_gerbong;
+      gerbong2.push(jml_gerbong_test);
       get("#jml_gerbong2").innerHTML = jml_gerbong_test;
     },
   });
@@ -72,6 +76,7 @@ function getSeat() {
 
 getSeat();
 jml_gerbong_test();
+
 get("#total").innerHTML = change;
 const Toast = Swal.mixin({
   toast: true,
@@ -187,7 +192,7 @@ function pesanTiket(pulang) {
     }`;
     id_kereta_pulang = get("#id_kereta_pulang").value;
     tanggal_berangkat_pulang = get("#tglPulang").value;
-    seat2 = get("#seat2").value
+    seat2 = get("#seat2").value;
   }
   let data = {
     no_pesanan: no_pesanan,
@@ -327,240 +332,6 @@ function pesanTiket(pulang) {
   });
 }
 
-function postTiket(pulang) {
-  let no_pesan = new Date().getTime();
-  let no_pesanan =
-    no_pesan.toString() + get("#id_kereta").value + get("#id_user").value;
-  let data = {
-    no_pesanan: no_pesanan,
-    id_kereta: get("#id_kereta").value,
-    nama_kereta: get("#namaKereta").value,
-    jumlah_tiket: get("#jumlahTiket").value,
-    stasiun_awal: get("#stasiunAwal").value,
-    stasiun_akhir: get("#stasiunAkhir").value,
-    dewasa: get("#dewasa").value,
-    bayi: get("#anak").value,
-    tanggal_berangkat: get("#tglBerangkat").value,
-    tanggal_pulang: pulang,
-    total: get("#totalOy").textContent,
-    id_pemesan: get("#id_user").value,
-    nama_pemesan: get("#namaPemesan").value,
-    email_pemesan: get("#emailPemesan").value,
-  };
-
-  $.ajax({
-    url: "http://localhost/ktrains-rest/api/TiketOrder",
-    type: "post",
-    dataType: "json",
-    data: data,
-    success: function (result) {
-      get("#load").style.opacity = "0";
-      get("#load").style.pointerEvent = "none";
-      get("#load").style.position = "absolute";
-      get("#receiptTitle").style.pointerEvent = "visible";
-      get("#receiptTitle").style.position = "static";
-      get("#receiptTitle").style.opacity = "1";
-      get("#receiptCard").style.pointerEvent = "visible";
-      get("#receiptCard").style.position = "static";
-      get("#receiptCard").style.opacity = "1";
-      if (result.status) {
-        let string;
-        if (pulang == 0) {
-          string =
-            `
-  <div class="card p-3 shadow" style="background-color: #fff">
-  <div class="row">
-    <div class="col-12 text-center">Receipt</div>
-  </div>
-  <div class="row mt-3">
-    <div class="col-5">No. Pesanan</div>
-    <div class="col-2 text-center">:</div>
-    <div class="col-5 text-right">` +
-            result.data.no_pesanan +
-            `</div>
-  </div>
-  <div class="row mt-1">
-    <div class="col-5">Nama Kereta</div>
-    <div class="col-2 text-center">:</div>
-    <div class="col-5 text-right">` +
-            result.data.nama_kereta +
-            `</div>
-  </div>
-  <div class="row mt-1">
-    <div class="col-5">Jumlah Tiket</div>
-    <div class="col-2 text-center">:</div>
-    <div class="col-5 text-right">` +
-            result.data.jumlah_tiket +
-            `</div>
-  </div>
-  <div class="row mt-1">
-    <div class="col-5">Stasiun Awal</div>
-    <div class="col-2 text-center">:</div>
-    <div class="col-5 text-right">` +
-            result.data.stasiun_awal +
-            `</div>
-  </div>
-  <div class="row mt-1">
-    <div class="col-5">Stasiun Akhir</div>
-    <div class="col-2 text-center">:</div>
-    <div class="col-5 text-right">` +
-            result.data.stasiun_akhir +
-            `</div>
-  </div>
-  <div class="row mt-1">
-    <div class="col-5">Dewasa</div>
-    <div class="col-2 text-center">:</div>
-    <div class="col-5 text-right">` +
-            result.data.dewasa +
-            `</div>
-  </div>
-  <div class="row mt-1">
-    <div class="col-5">Anak</div>
-    <div class="col-2 text-center">:</div>
-    <div class="col-5 text-right">` +
-            result.data.bayi +
-            `</div>
-  </div>
-  <div class="row mt-1">
-    <div class="col-5">Tanggal Berangkat</div>
-    <div class="col-2 text-center">:</div>
-    <div class="col-5 text-right">` +
-            result.data.tanggal_berangkat +
-            `</div>
-  </div>
-  <div class="row mt-1">
-    <div class="col-5">Total</div>
-    <div class="col-2 text-center">:</div>
-    <div class="col-5 text-right">` +
-            formatter.toRupiah(result.data.total) +
-            `</div>
-  </div>
-  <div class="row justify-content-center mt-3">
-    <div class="col-12 text-center">
-      <svg id="barcodeTiket"></svg>
-    </div>
-  </div>
-  <div class="row justify-content-center mt-3">
-    <div class="col-12 text-center">
-      <span class="text-muted"
-        >Note: harap selesaikan pembayaran</span
-      >
-    </div>
-  </div>
-</div>
-  `;
-        } else {
-          string =
-            `
-            <div class="card p-3 shadow" style="background-color: #fff">
-            <div class="row">
-              <div class="col-12 text-center">Receipt</div>
-            </div>
-            <div class="row mt-3">
-              <div class="col-5">No. Pesanan</div>
-              <div class="col-2 text-center">:</div>
-              <div class="col-5 text-right">` +
-            result.data.no_pesanan +
-            `</div>
-            </div>
-            <div class="row mt-1">
-              <div class="col-5">Nama Kereta</div>
-              <div class="col-2 text-center">:</div>
-              <div class="col-5 text-right">` +
-            result.data.nama_kereta +
-            `</div>
-            </div>
-            <div class="row mt-1">
-              <div class="col-5">Jumlah Tiket</div>
-              <div class="col-2 text-center">:</div>
-              <div class="col-5 text-right">` +
-            result.data.jumlah_tiket +
-            `</div>
-            </div>
-            <div class="row mt-1">
-              <div class="col-5">Stasiun Awal</div>
-              <div class="col-2 text-center">:</div>
-              <div class="col-5 text-right">` +
-            result.data.stasiun_awal +
-            `</div>
-            </div>
-            <div class="row mt-1">
-              <div class="col-5">Stasiun Akhir</div>
-              <div class="col-2 text-center">:</div>
-              <div class="col-5 text-right">` +
-            result.data.stasiun_akhir +
-            `</div>
-            </div>
-            <div class="row mt-1">
-              <div class="col-5">Dewasa</div>
-              <div class="col-2 text-center">:</div>
-              <div class="col-5 text-right">` +
-            result.data.dewasa +
-            `</div>
-            </div>
-            <div class="row mt-1">
-              <div class="col-5">Anak</div>
-              <div class="col-2 text-center">:</div>
-              <div class="col-5 text-right">` +
-            result.data.bayi +
-            `</div>
-            </div>
-            <div class="row mt-1">
-              <div class="col-5">Tanggal Berangkat</div>
-              <div class="col-2 text-center">:</div>
-              <div class="col-5 text-right">` +
-            result.data.tanggal_berangkat +
-            `</div>
-            </div>
-            <div class="row mt-1">
-              <div class="col-5">Tanggal Pulang</div>
-              <div class="col-2 text-center">:</div>
-              <div class="col-5 text-right">` +
-            result.data.tanggal_pulang +
-            `</div>
-            </div>
-            <div class="row mt-1">
-              <div class="col-5">Total</div>
-              <div class="col-2 text-center">:</div>
-              <div class="col-5 text-right">` +
-            formatter.toRupiah(result.data.total) +
-            `</div>
-            </div>
-            <div class="row justify-content-center mt-3">
-              <div class="col-12 text-center">
-                <svg id="barcodeTiket"></svg>
-              </div>
-            </div>
-            <div class="row justify-content-center mt-3">
-              <div class="col-12 text-center">
-                <span class="text-muted"
-                  >Note: harap selesaikan pembayaran</span
-                >
-              </div>
-            </div>
-          </div>
-            `;
-        }
-        get("#tiketPlace").innerHTML = string;
-        setTimeout(() => {
-          JsBarcode("#barcodeTiket", result.data.no_pesanan, {
-            format: "codabar",
-            lineColor: "#000",
-            width: 1.7,
-            height: 40,
-            displayValue: true,
-            background: "#fff",
-            fontSize: "17",
-          });
-        }, 200);
-      } else {
-        get("#beliTiket").style.display = "block";
-        get("#lihatDetail").style.display = "none";
-        alert(result.message);
-      }
-    },
-  });
-}
 
 // * new one
 const notAvailable = data;
@@ -571,8 +342,18 @@ setTimeout(() => {
     createTemplateChosseSeat2("A", notAvailable2, "");
   }
 
-  const jml_gerbong = get("#jml_gerbong").textContent;
-  const jml_gerbong2 = get("#jml_gerbong2").textContent;
+  let jml_gerbong = get("#jml_gerbong").textContent;
+  let jml_gerbong2 = get("#jml_gerbong2").textContent;
+  gerbong.forEach(g => {
+    jml_gerbong = g;
+  })
+  if (get("#tglPulang")) {
+    gerbong2.forEach(g => {
+      jml_gerbong2 = g;
+    })
+  }
+  console.log(jml_gerbong2);
+  console.log(jml_gerbong);
   get("#gerbongSebelum").addEventListener("click", function () {
     this.style.opacity = 0.8;
     const checkThisOut = parseInt(get("#gerbongIni").textContent) - 1;
